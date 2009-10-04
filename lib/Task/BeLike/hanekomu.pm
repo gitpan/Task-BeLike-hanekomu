f06df702c31d1cb1c37e5ecf927f0fb80f0fa8da
Marcel Grünauer <marcel@cpan.org>
2009-10-04 10:23:13 +0000

    Import of MARCEL/Task-BeLike-hanekomu-0.01 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-hanekomu
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MARCEL/Task-BeLike-hanekomu-0.01.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/hanekomu.pm
@@ -0,0 +1,59 @@
+package Task::BeLike::hanekomu;
+
+use 5.008;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Task::BeLike::hanekomu - install modules I like
+
+=head1 SYNOPSIS
+
+# None
+
+=head1 DESCRIPTION
+
+This L<Task> installs modules that I need to work with. They are listed in
+this distribution's C<Makefile.PL>.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN site
+near you. Or see L<http://search.cpan.org/dist/Task-BeLike-hanekomu/>.
+
+The development version lives at
+L<http://github.com/hanekomu/task-belike-hanekomu/>. Instead of sending
+patches, please fork this project using the standard git and github
+infrastructure.
+
+=head1 AUTHORS
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2009 by Marcel GrE<uuml>nauer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
